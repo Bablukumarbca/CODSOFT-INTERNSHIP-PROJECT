7a4f77953223cf581cadb713ed6cbd548aa2eb82
Bablu kumar yadav <128819337+Bablukumarbca@users.noreply.github.com>
2024-09-09 19:55:57 +0530

    Atm interface

---
+++ interface1.java
@@ -0,0 +1,10 @@
+package CODSOFT;
+
+public interface interface1
+{
+	public void register();
+	public boolean login();
+	public void deposit();
+	public void withdraw();
+	public void checkBalance();
+}
